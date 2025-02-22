d1c46ef3ab4ba2b4c5e93b25b041e159ee4ffae4
avawingfield <avelinewingfield@gmail.com>
2025-02-22 16:29:48 -0500

    Kubernetes Exposes /healthz

--- handlers/handler.go
+++ handlers/handler.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"time"
 )
 
 type Handler struct {
@@ -13,19 +15,30 @@ type Config struct {
 }
 
 func NewHandler(c *Config) {
-	//h := &Handler{}
-	c.R.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"hello": "world"})
-	})
+	h := &Handler{}
+	c.R.GET("/", h.HelloWorld)
 
 	c.R.GET("/metrics", gin.WrapH(promhttp.Handler()))
-	c.R.GET("/liveness", func(c *gin.Context) {
-		c.JSON(200, gin.H{"liveness": "true"})
-	})
+	c.R.GET("/health", h.Healthz)
+	c.R.GET("/healthz", h.Healthz)
 
 	eth := c.R.Group("/eth")
 	eth.GET("/balance/:address", func(c *gin.Context) {
 		c.JSON(200, gin.H{"balance": "0x" + c.Param("address")})
 	})
 
+}
+
+func (h *Handler) HelloWorld(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "hello world"})
+}
+
+func (h *Handler) Healthz(c *gin.Context) {
+	started := time.Now()
+	duration := time.Now().Sub(started)
+	if duration.Seconds() > 10 {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": duration.Seconds()})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK})
+	}
 }
